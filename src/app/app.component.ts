2d3f9e8b63556d5f178f6500b63e9cef006f676f
ascaryaaa <allaamascarya@gmail.com>
2024-02-15 09:48:13 +0700

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: `
+    <h1>learn-angular</h1>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'learn-angular';
