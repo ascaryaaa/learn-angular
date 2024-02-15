33f0180dbda8c5161676cffa6c97719cab33b2fa
ascaryaaa <allaamascarya@gmail.com>
2024-02-15 14:08:39 +0700

    add component standalone hero-detail

--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroRoutingModule } from './hero-routing.module';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    HeroListComponent
+    HeroListComponent,
+    HeroDetailComponent
   ],
   imports: [
     CommonModule,
